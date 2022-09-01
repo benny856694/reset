f34236ce6fac014201557bdca830732be285a6fc
Benny Shen <shenbinsc@qq.com>
2022-09-02 00:34:40 +0800

    login button enable state

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ const username = "root";
 const password = "admin";
 const echoEnabled = true;
 
-enum DeviceType { oldModel, newModel }
+enum DeviceType { oldModel, newModel, otherModel }
 
 final counterProvider = StateProvider((ref) => 0);
 final deviceTypeProvider = StateProvider((_) => DeviceType.oldModel);
@@ -29,15 +29,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends HookConsumerWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
 
   final String title;
+
+  final ipAddressExp = RegExp(
+      r'\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b');
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -46,6 +49,28 @@ class MyHomePage extends HookConsumerWidget {
     final isLoginButtonEnabled = useState(false);
     final ipAddressController = useTextEditingController();
     final isLoggedin = ref.watch(isLoggedinProvider);
+    final passwordController = useTextEditingController();
+
+    bool enableLogin() {
+      var enabled = true;
+      enabled = enabled && ipAddressExp.hasMatch(ipAddressController.text);
+      var dt = ref.read(deviceTypeProvider.notifier);
+      print('device type: $dt');
+      if (dt.state == DeviceType.otherModel) {
+        enabled = enabled && passwordController.text.isNotEmpty;
+      }
+      print('enable: $enabled');
+      return enabled;
+    }
+
+    ref.listen(
+      deviceTypeProvider,
+      (previous, next) {
+        print('new value: $next');
+        //isLoginButtonEnabled.value = enableLogin();
+      },
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -63,9 +88,25 @@ class MyHomePage extends HookConsumerWidget {
                   label: Text("Ip Address:"),
                 ),
                 onChanged: (value) {
-                  isLoginButtonEnabled.value = value.isNotEmpty;
+                  isLoginButtonEnabled.value = enableLogin();
                 },
               ),
+              if (deviceType == DeviceType.otherModel)
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: passwordController,
+                        decoration: const InputDecoration(
+                          label: Text("Password:"),
+                        ),
+                        onChanged: (value) {
+                          isLoginButtonEnabled.value = enableLogin();
+                        },
+                      ),
+                    )
+                  ],
+                ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
                 child: Row(
@@ -84,6 +125,7 @@ class MyHomePage extends HookConsumerWidget {
                               onChanged: (value) {
                                 ref.read(deviceTypeProvider.notifier).state =
                                     value!;
+                                isLoginButtonEnabled.value = enableLogin();
                               },
                             ),
                             Text(
@@ -105,6 +147,14 @@ class MyHomePage extends HookConsumerWidget {
                     : null,
                 icon: const Icon(Icons.login),
                 label: !isLoggedin ? const Text('Login') : const Text("Logout"),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              ElevatedButton.icon(
+                onPressed: isLoggedin ? () {} : null,
+                icon: const Icon(Icons.restore),
+                label: const Text("Reset Config"),
               ),
               const SizedBox(
                 height: 8.0,
