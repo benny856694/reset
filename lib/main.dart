4930142d03e9ffd79c05d8bc1910fbc55b390105
shen bin <shenbinsc@qq.com>
2023-10-30 13:25:33 +0800

    cosmetic

--- lib/main.dart
+++ lib/main.dart
@@ -301,6 +301,7 @@ class MyHomePage extends HookConsumerWidget {
                     Text('选择脚本功能'),
                     Text('重置触屏密码功能'),
                     Text('自动保存IP地址'),
+                    Text('Telnet使用CTelnet'),
                   ],
                 );
               },
